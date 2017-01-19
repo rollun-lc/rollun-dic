55fc4b7279dde1cb37983fdced234037ebafdc4c
victorynox <it.professor02@gmail.com>
2017-01-19 15:31:45 +0200

    fix format to psr

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 global $argv;
 
-error_reporting( E_ALL | E_STRICT );
+error_reporting(E_ALL | E_STRICT);
 
 // Change to the project root, to simplify resolving paths
 chdir(dirname(__DIR__));
 
 $appEnv = getenv("APP_ENV");
-if($appEnv != 'dev') {
+if ($appEnv != 'dev') {
     echo "You cannot start test if environment var APP_ENV not set in dev!";
     exit(1);
 }
